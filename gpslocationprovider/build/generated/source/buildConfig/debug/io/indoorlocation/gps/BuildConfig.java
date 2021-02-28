8ea8ea85c4f75c667d85b4f7ffa6299bf2bcc187
Abdullah AlMarzoq <45847779+Bomrzoq@users.noreply.github.com>
2021-02-28 12:28:21 +0300

    Add files via upload

---
+++ gpslocationprovider/build/generated/source/buildConfig/debug/io/indoorlocation/gps/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package io.indoorlocation.gps;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "io.indoorlocation.gps";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "io.indoorlocation.gps";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 100040;
+  public static final String VERSION_NAME = "1.0.4";
+}
